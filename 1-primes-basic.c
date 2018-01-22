0309f9425a92afb6f5c64ca9e70bffef2e82e18d
Prakash B Hegade <prakash.hegade@gmail.com>
2018-01-23 00:57:22 +0530

    Update 1-primes-basic.c

--- 1-primes-basic.c
+++ 1-primes-basic.c
@@ -1,3 +1,10 @@
+/*
+A prime number is a number that is divisible only 1 and by itself
+Example: 2, 3, 5, 7, 11 etc
+As by definition we can see that prime number is generally defined to be any positive number that has 
+exactly two distinct positive integer divisors. For the same reason 1 is not considered as a prime number. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,10 +15,13 @@ int main()
 	scanf("%d%d",&start, &end);
 
 	printf("Prime Numbers between %d and %d is :\n", start, end);
-	for(i=start; i<=end; i++) {
+	for(i=start; i<=end; i++)
+	{
 		flag=0;
-		for(j=2; j<i; j++) {
-			if(i%j==0) {
+		for(j=2; j<i; j++)
+		{
+			if(i%j==0)
+			{
 				flag = 1;
 				break;
 			}
