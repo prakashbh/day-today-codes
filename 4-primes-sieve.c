ce3d24dffcc80491c7a0f9a8a33c3fc4f5f92422
Prakash B Hegade <prakash.hegade@gmail.com>
2018-01-23 13:41:21 +0530

    Create 4-primes-sieve.c

---
+++ 4-primes-sieve.c
@@ -0,0 +1,34 @@
+/*
+Reference from Wikipedia:
+The algorithm finds the prime by iteratively marking as composite (i.e., not prime) the multiples of each prime, 
+starting with the first prime number, 2. The multiples of a given prime are generated as a sequence of numbers 
+starting from that prime, with constant difference between them that is equal to that prime.
+This is the sieve's key distinction from using trial division to sequentially test each candidate number for 
+divisibility by each prime.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    long int i,j;
+    int n = 100;
+    int *primes;
+
+    primes = malloc(sizeof(int) * n);
+    memset(primes, 1, n*sizeof(int));
+
+    for (i = 2;i < n; i++)
+        if (primes[i])
+            for (j = i;i * j < n; j++)
+                primes[i * j] = 0;
+
+    printf("\nPrime numbers in range 1 to 100 are: \n");
+    for (i = 2;i < n; i++){
+        if (primes[i])
+            printf("%ld ", i);
+    }
+
+return 0;
+}
