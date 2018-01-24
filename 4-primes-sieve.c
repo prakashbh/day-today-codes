670dcf596567057df2ec09491d6684dafcc13afa
Prakash B Hegade <prakash.hegade@gmail.com>
2018-01-24 23:51:34 +0530

    Update 4-primes-sieve.c

--- 4-primes-sieve.c
+++ 4-primes-sieve.c
@@ -16,17 +16,17 @@ int main(){
     int n = 100;
     int *primes;
 
-    primes = malloc(sizeof(int) * n);
+    primes = (int *)malloc(sizeof(int) * n);
     memset(primes, 1, n*sizeof(int));
 
     for (i = 2;i < n; i++){
         if (primes[i]) {
-            for (j = i;i * j < n; j++)
+            for (j = i; i * j < n; j++)
                 primes[i * j] = 0;
         }
     }
 
-    printf("\nPrime numbers in range 1 to 100 are: \n");
+    printf("\nPrime numbers in range 1 to %d are: \n", n);
     for (i = 2;i < n; i++){
         if (primes[i])
             printf("%ld ", i);
