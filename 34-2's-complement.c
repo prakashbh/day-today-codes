a73d6726ba343d6a9605d46b94cad2b835d8c239
Prakash B Hegade <prakash.hegade@gmail.com>
2018-02-27 22:13:48 +0530

    Update 34-2's-complement.c

--- 34-2's-complement.c
+++ 34-2's-complement.c
@@ -4,7 +4,7 @@ int main()
 {
     int num = 7;
 
-    /// Two ways to get two's compliment
+    /// Two ways to get two's complement
     printf("%d\n", -num);
     printf("%d\n", ~num + 1);
     return 0;
