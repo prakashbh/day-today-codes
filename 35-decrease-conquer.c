e6b05190b8d3d98259c10056222d392a5d2eb724
Prakash B Hegade <prakash.hegade@gmail.com>
2018-03-06 11:24:08 +0530

    Create 35-decrease-conquer.c

---
+++ 35-decrease-conquer.c
@@ -0,0 +1,61 @@
+/*
+
+Uses the definition of n! to generate the permutations.
+
+Idea:
+-----
+Remove each item from the given n items one at a time and
+append it to remaining (n-1)! permutations.
+
+Efficiency:
+-----------
+O(n!) and as well we have expensive swaps
+
+Strategy used:
+--------------
+Decrease and Conquer(decrease by 1)
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global n
+int gn;
+
+void permute(int a[], int n)
+{
+    if (n == 1)
+    {
+        int i;
+        for(i = 0; i < gn; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+        return;
+    }
+
+    int i;
+    int temp;
+    for(i = 0; i < n; i++)
+    {
+        temp = a[i];
+        a[i] = a[n-1];
+        a[n-1] = temp;
+        permute(a, n-1);
+        temp = a[i];
+        a[i] = a[n-1];
+        a[n-1] = temp;
+
+    }
+}
+
+int main()
+{
+    int a[5] = {1, 2, 3};
+    int n = 3;
+    gn = n;
+
+    permute(a, n);
+
+    return 0;
+}
